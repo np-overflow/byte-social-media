e47c496964bd3603d261d5fb55c435ff0ae4b95a
Joel Tio <joeltiojinhon@gmail.com>
2018-11-04 12:09:41 +0800

    Add react packages and base code

---
+++ frontend/src/client.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const MainLayout = () => (
+    <div></div>
+)
+
+const app = document.getElementById("app")
+ReactDOM.render(<MainLayout />, app)
---
+++ frontend/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    context: __dirname,
+    entry: './src/client.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env'],
+                    plugins: [
+                        '@babel/plugin-syntax-dynamic-import',
+                        'react-html-attrs',
+                        'react-css-modules'
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader?modules']
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/assets/js/',
+        publicPath: '/assets/js/',
+        filename: 'client.min.js'
+    }
+}
