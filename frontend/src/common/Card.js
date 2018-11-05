eb2759419a13b15893c7bb8618860d0cf8cdcbc9
Joel Tio <joeltiojinhon@gmail.com>
2018-11-05 20:24:15 +0800

    Add Card and CardCaption into common

---
+++ frontend/src/common/Card.js
@@ -0,0 +1,28 @@
+import React from "react"
+import invariant from "invariant"
+
+import styles from "./card.css"
+import MediaBanner from "./MediaBanner";
+
+const Card = props => {
+    invariant(props.children.length === 2, "Card should have 2 children elements")
+    invariant(props.iconSrc, "Card should have an iconSrc prop")
+
+    const mediaBannerProps = {
+        bright: props.titleBright,
+        dark: props.titleDark,
+        bgColor: props.titleBgColor,
+        iconSrc: props.iconSrc
+    }
+
+    return (
+        <div className={styles['card-container'] + " pb-2"}>
+            <MediaBanner {...mediaBannerProps}>
+                {props.title}
+            </MediaBanner>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card
