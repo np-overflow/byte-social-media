8db6166ee57cc08382b9dfabf3102565a68c0a59
Joel Tio <joeltiojinhon@gmail.com>
2018-11-05 15:22:32 +0800

    Add MediaBanner into common

---
+++ frontend/src/common/MediaBanner.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import styles from "./mediaBanner.css"
+import SmallName from "./SmallName";
+
+const MediaBanner = props => {
+    let smallNameProps = {
+        bright: props.bright,
+        dark: props.dark
+    }
+
+    return (
+        <div style={{ backgroundColor: props.bgColor }}
+            className="container-fluid p-1 d-flex flex-row align-items-center">
+            <img className={styles.icon + " p-1"} src={props.iconSrc} />
+            <SmallName {...smallNameProps}>{props.children}</SmallName>
+        </div>
+    )
+}
+
+export default MediaBanner
