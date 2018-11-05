2e61eb85350db498ceae07ce2c01bca11f13eb44
Joel Tio <joeltiojinhon@gmail.com>
2018-11-05 15:30:49 +0800

    Add more spacing between media icon and text

--- frontend/src/common/MediaBanner.js
+++ frontend/src/common/MediaBanner.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import styles from "./mediaBanner.css"
-import SmallName from "./SmallName";
+import SmallName from "./SmallName"
 
 const MediaBanner = props => {
     let smallNameProps = {
@@ -12,7 +12,7 @@ const MediaBanner = props => {
     return (
         <div style={{ backgroundColor: props.bgColor }}
             className="container-fluid p-1 d-flex flex-row align-items-center">
-            <img className={styles.icon + " p-1"} src={props.iconSrc} />
+            <img className={styles.icon + " p-1 pr-2"} src={props.iconSrc} />
             <SmallName {...smallNameProps}>{props.children}</SmallName>
         </div>
     )
