a18ae10d62c8c79b4fe06f069308e45942559010
Joel Tio <joeltiojinhon@gmail.com>
2018-11-05 11:11:28 +0800

    Add margin: 0 style from small name

--- frontend/src/common/SmallName.js
+++ frontend/src/common/SmallName.js
@@ -3,13 +3,11 @@ import React from "react"
 import styles from "./smallName.css"
 
 const SmallName = props => {
-    let textStyle;
+    let textStyle = styles.name
     if (props.bright) {
-        textStyle = styles.bright
+        textStyle += " " + styles.bright
     } else if (props.dark) {
-        textStyle = styles.dark
-    } else {
-        textStyle = ""
+        textStyle = " " + styles.dark
     }
 
     return <p className={textStyle}>{props.children}</p>
