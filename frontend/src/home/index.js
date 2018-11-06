3f7e824a3172838e887a65d21335128e1d6471ba
Joel Tio <joeltiojinhon@gmail.com>
2018-11-06 20:52:16 +0800

    Add HashtagTitle into HomeLayout with styles

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 
+import styles from "./index.css"
+import HashtagTitle from "./HashtagTitle";
+
 const HomeLayout = () => (
-    <div></div>
+    <div>
+        <HashtagTitle className={styles.banner} />
+    </div>
 )
 
 export default HomeLayout
