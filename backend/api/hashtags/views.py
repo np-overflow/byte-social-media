3e37e363247c31ae30917d295dcaaa3040b07810
Joel Tio <joeltiojinhon@gmail.com>
2018-11-07 22:41:26 +0800

    Add hashtag view code and doc

--- backend/api/hashtags/views.py
+++ backend/api/hashtags/views.py
@@ -2,4 +2,12 @@ from django.http import JsonResponse
 
 
 def main(request):
-    return JsonResponse({})
+    """API for retrieving the list of hashtags
+    
+    This view returns a JsonResponse of the hashtags. The list should include
+    the hashtags starting with a hashtag (#). For example:
+
+        ["#foo", "#bar"]
+
+    """
+    return JsonResponse([], safe=False)
