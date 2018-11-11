58e00e64859e584c6520b60c9d0e0b2e5962e6ae
Joel Tio <joeltiojinhon@gmail.com>
2018-11-11 17:21:47 +0800

    Add Feed template into home layout

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 
 import styles from "./index.css"
-import HashtagTitle from "./HashtagTitle";
+import HashtagTitle from "./HashtagTitle"
+import Feed from "./Feed"
 
 const HomeLayout = () => (
     <div>
         <HashtagTitle className={styles.banner} />
+        <Feed />
     </div>
 )
 
--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,3 +1,17 @@
+function construct_ws_url(endpoint) {
+    let loc = window.location, new_uri;
+    if (loc.protocol === "https:") {
+        new_uri = "wss:"
+    } else {
+        new_uri = "ws:"
+    }
+
+    new_uri += "//" + loc.host
+    new_uri += loc.pathname + endpoint
+
+    return new_uri
+}
+
 export const FACEBOOK_SRC = "/assets/imgs/facebook_logo/flogo-72.svg"
 export const FACEBOOK_BG_COLOR = "#3351a2"
 
@@ -7,6 +21,5 @@ export const TWITTER_BG_COLOR = "#1D8DEE"
 export const INSTAGRAM_SRC = "/assets/imgs/instagram_logo/instagram-logo.svg"
 export const INSTAGRAM_BG_COLOR = "#CD1662"
 
-const API_ENDPOINT = "/api/"
-export const API_ENDPOINT_HASHTAGS = API_ENDPOINT + "hashtags/"
-export const API_SOCKET_ENDPOINT_POSTS = "gateway/"
+export const API_ENDPOINT_HASHTAGS = "/api/hashtags/"
+export const API_SOCKET_ENDPOINT_POSTS = construct_ws_url("ws/posts/")
