f39df17db5ca2c496975148e48bf6f14578d8aa5
Joel Tio <joeltiojinhon@gmail.com>
2018-11-11 15:31:37 +0800

    Fix posts models unassigned foreign key The foreign key to media in posts was not given a name

--- backend/api/posts/models.py
+++ backend/api/posts/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     author = models.CharField(max_length=100)
     caption = models.CharField(max_length=400)
     isApproved = models.BooleanField(null=True)
-    models.ForeignKey(Media, on_delete=models.PROTECT, null=True)
+    media = models.ForeignKey(Media, on_delete=models.PROTECT, null=True)
