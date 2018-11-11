cbc1ad03b1859cacbd50336cd57164cd6926abd0
Joel Tio <joeltiojinhon@gmail.com>
2018-11-11 23:48:36 +0800

    Move model to dict functions to models.py

--- backend/api/posts/signals.py
+++ backend/api/posts/signals.py
@@ -10,28 +10,12 @@ from . import settings
 channel_layer = get_channel_layer()
 
 
-def post_to_dict(post_instance):
-    media = None
-    if post_instance.media:
-        media = {
-            "type": post_instance.media.kind,
-            "src": post_instance.media.src
-        }
-
-    return {
-        "author": post_instance.author,
-        "caption": post_instance.caption,
-        "isApproved": post_instance.isApproved,
-        "media": media
-    }
-
-
 @receiver(post_save, sender=models.Post)
 def update_socket_group(sender, instance, **kwargs):
     async_to_sync(channel_layer.group_send)(
         settings.POSTS_GROUP_NAME,
         {
             "type": "new_post",
-            "text": post_to_dict(instance),
+            "text": models.post_to_dict(instance),
         }
     )
