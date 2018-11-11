04a1498aea322927bba73ced4a2e3aeb3a014637
Joel Tio <joeltiojinhon@gmail.com>
2018-11-12 02:34:47 +0800

    Add Feed into home index

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import styles from "./index.css"
 import HashtagTitle from "./HashtagTitle"
-import Feed from "./Feed"
+import Feed from "../common/Feed"
+
+import { API_SOCKET_ENDPOINT_POSTS } from "../settings"
 
 const HomeLayout = () => (
     <div>
         <HashtagTitle className={styles.banner} />
-        <Feed />
+        <Feed endpoint={API_SOCKET_ENDPOINT_POSTS} />
     </div>
 )
 
