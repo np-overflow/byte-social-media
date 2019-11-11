30010574eac8ca1849bbb6778927c4e6dbd1a837
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-11-11 22:49:49 +0800

    Added simple telegram route

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 import requests
+from tbot import TBot
 from pprint import pprint
 app = Flask(__name__)
 
 TOKEN = '827940689:AAFH0jE2qa6wvid-3my020PSv1sRO_F5bDM'
+WEBHOOK_URL = '94.237.74.114/telegram'
+
+bot = TBot(TOKEN)
 
 @app.route('/telegram', method=['POST'])
 def telegram():
@@ -11,4 +15,4 @@ def telegram():
     return ''
 
 if __name__ == "__main__":
-    
+    bot.set_webhook(WEBHOOK_URL)
