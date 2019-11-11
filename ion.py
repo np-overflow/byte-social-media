1a9db123dcedf2772196a38c7162d263c0a01e58
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-11-12 00:07:46 +0800

    hopefully fixed a bug

--- ion.py
+++ ion.py
@@ -5,8 +5,9 @@ TOKEN = '827940689:AAFH0jE2qa6wvid-3my020PSv1sRO_F5bDM'
 BOT_URL = f'https://api.telegram.org/bot{TOKEN}/'
 HOST = '94.237.74.114'
 URL = f'/telegram/{TOKEN}'
-WEBHOOK_URL = f'{HOST}/{URL}'
+WEBHOOK_URL = f'https://{HOST}/{URL}'
 
 bot = TBot(TOKEN)
 
 x = lambda: requests.get(BOT_URL + 'setWebhook', json={'url': WEBHOOK_URL})
+y = lambda json: requests.post('WEBHOOK_URL', json=json)
