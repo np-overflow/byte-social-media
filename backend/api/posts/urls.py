e77122ba6c21d972ce96cd380914dca197c3e8d5
Joel Tio <joeltiojinhon@gmail.com>
2019-11-12 10:42:30 +0800

    Fix incorrect reference to telegram bot token

--- backend/api/posts/urls.py
+++ backend/api/posts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.home),
     path("manage/", views.admin),
     path("manage/create", views.admin_input, name="admin-create"),
-    path(f"telegram/{settings.token}", views.telegram_webhook),
+    path(f"telegram/{settings.TELEGRAM_BOT_TOKEN}", views.telegram_webhook),
 ]
